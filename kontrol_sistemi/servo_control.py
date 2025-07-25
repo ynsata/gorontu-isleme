60ce2c636802c5925a0509e1aea161f0a2cf5b00
ynsata <ynsata52@gmail.com>
2025-07-25 13:47:28 +0300

    .

--- kontrol_sistemi/servo_control.py
+++ kontrol_sistemi/servo_control.py
@@ -52,7 +52,7 @@ def norm_px(px, res):
 
 def px_to_angle(px, res, fov):
     x, y = norm_px(px,res)
-    angle_x = x * fov[0] / 2
+    angle_x = -x * fov[0] / 2
     angle_y = -y * fov[1] / 2
 
     return (angle_x, angle_y)
