aa4d207c4a809f23c90718440d00b0b9d98c5fcb
ynsata <ynsata52@gmail.com>
2025-07-25 13:44:29 +0300

    .

--- kontrol_sistemi/servo_control.py
+++ kontrol_sistemi/servo_control.py
@@ -72,10 +72,10 @@ def handle_servo_direction(servo_yatay, servo_dikey, x, y,time_sec):
     # print(f"pid_y={pid_output_yatay}	")
     # if abs(pid_output_yatay) > ESIK_YATAY:
     yeni_yatay = max(0, min(270, round(mevcut_yatay + pid_output_yatay))) 
-    print(f"{yeni_yatay=}",f"{hedef_yatay=}")
+    print(f"{yeni_yatay=}",f"{hedef_yatay=}",f"{mevcut_yatay}")
         # print(f"yeni_yatay={yeni_yatay}")
         # if yeni_yatay != mevcut_yatay:
-    servo_yatay.set_angle(yeni_yatay, smooth=True, step=1, delay=0.005)
+    # servo_yatay.set_angle(yeni_yatay, smooth=True, step=1, delay=0.005)
         #     # print(
         #         # f"[YATAY] mevcut={mevcut_yatay:.2f}, hedef={hedef_yatay:.2f}, çıktı={pid_output_yatay:.2f}, yeni={yeni_yatay}"
         #     # )
