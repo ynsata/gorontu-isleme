1819c7112b809de0b1ce2e82cdef4032691824f6
ynsata <ynsata52@gmail.com>
2025-07-25 13:57:38 +0300

    .

--- kontrol_sistemi/servo_control.py
+++ kontrol_sistemi/servo_control.py
@@ -75,7 +75,7 @@ def handle_servo_direction(servo_yatay, servo_dikey, x, y,time_sec):
     print(f"{yeni_yatay=}",f"{hedef_yatay=}",f"{mevcut_yatay}")
         # print(f"yeni_yatay={yeni_yatay}")
         # if yeni_yatay != mevcut_yatay:
-    servo_yatay.set_angle(int(round(yeni_yatay)), smooth=True, step=1, delay=0.005)
+    servo_yatay.set_angle(int(round(yeni_yatay)), smooth=False, step=1, delay=0.005)
         #     # print(
         #         # f"[YATAY] mevcut={mevcut_yatay:.2f}, hedef={hedef_yatay:.2f}, çıktı={pid_output_yatay:.2f}, yeni={yeni_yatay}"
         #     # )
@@ -100,7 +100,7 @@ def handle_servo_direction(servo_yatay, servo_dikey, x, y,time_sec):
     yeni_dikey = max(110, min(170, dikey_komut))
     #     # print(f"yeni_dikey={yeni_dikey}")
     #     if yeni_dikey != mevcud_dikey:
-    servo_dikey.set_angle(int(round(yeni_dikey)), smooth=True, step=1, delay=0.004)
+    servo_dikey.set_angle(int(round(yeni_dikey)), smooth=False, step=1, delay=0.004)
     #         print(
     #             # f"[DİKEY] mevcud={mevcud_dikey:.2f}, hedef={hedef_dikey:.2f} (yazılımsal {pid_output_dikey:.2f}), çıktı={pid_output_dikey:.2f}, yeni={yeni_dikey}"
     #         )
